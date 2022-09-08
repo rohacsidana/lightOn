dd5db94ad5665960da4610dc16b19c46078ea3f4
rohacsidana <rohacsi.dana@gmail.com>
2022-09-08 23:51:25 +0200

    kodduplikálás javítás

--- script.js
+++ script.js
@@ -1,41 +1,23 @@
-
-
 let sorHossz;
 let osztalyJelenleg;
 let felKapcsolva = "green";
 let leKapcsolva = "yellow";
 
-
-
-
-
-$(function(){
+$(function () {
     gombok()
 })
 
 function gombok() {
-
-    $("#2").on("click", function () {
-        palya(4, "x2")
-        osztalyJelenleg = "x2";
-        sorHossz = 2;
-    })
-    $("#3").on("click", function () {
-        palya(9, "x3")
-        osztalyJelenleg = "x3";
-        sorHossz = 3;
-    })
-    $("#4").on("click", function () {
-        palya(16, "x4")
-        osztalyJelenleg = "x4";
-        sorHossz = 4;
-    })
-    $("#5").on("click", function () {
-        palya(25, "x5")
-        osztalyJelenleg = "x5";
-        sorHossz = 5;
-    })
-
+    let lampakSzama;
+    let gombok = $(".gomb")
+    for (let i = 0; i < gombok.length; i++) {
+        gombok[i].addEventListener("click", function () {
+            lampakSzama = parseInt(gombok[i].id) * parseInt(gombok[i].id)
+            osztalyJelenleg = `x${gombok[i].id}`
+            palya(lampakSzama, osztalyJelenleg)
+            sorHossz = parseInt(gombok[i].id);
+        })
+    }
 }
 
 function palya(n, osztaly) {
@@ -44,119 +26,86 @@ function palya(n, osztaly) {
     for (let index = 0; index < n; index++) {
         let random = Math.random()
         if (random <= 0.5 && zoldek < n / 2) {
-
             txt += `<div class="lampa ${index}" style="background-color: ${felKapcsolva};"></div>`
-
             zoldek += 1
         } else {
             txt += `<div class="lampa ${index}" style="background-color: ${leKapcsolva};"></div>`
-
-
         }
-
     }
-
-    document.getElementById("jatek").innerHTML = `<div id="fotarolo" class="${osztaly}"></div>`
-    document.getElementById("fotarolo").innerHTML = txt
+    $("#jatek").html(`<div id="fotarolo" class="${osztaly}"></div>`)
+    $("#fotarolo").html(txt)
     gombKatt()
 }
-//eseménykezelő a kattintásra, ellenőrzés h nyertünk-e 
-
 function gombKatt() {
-
-    let lampak = document.querySelectorAll(".lampa")
-
+    let lampak = $(".lampa")
     for (let i = 0; i < lampak.length; i++) {
 
-        lampak[i].addEventListener("click", hanyadikElem)
+        lampak[i].on("click", hanyadikElem)
     }
 }
 
 function hanyadikElem() {
     let index = 0
-
-    while (document.querySelectorAll(".lampa")[index] != event.target) {
+    while ($(".lampa")[index] != event.target) {
         index++
-
     }
-    let hanyadik = index
-
     valt(index)
-
 }
 
 
 function valt(n) { // a kattintott elem indexe 0-n ig
-
     let y = Math.floor(n / sorHossz);
     let x = n % sorHossz;
     szinValt(n);
     if (x != 0) {
         bal(n);
-
-
     }
     if (x != sorHossz - 1) {
         jobb(n);
-
     }
     if (y != 0) {
         fent(n)
-
     }
     if (y + 1 != sorHossz) {
-
         lent(n);
-
     }
     ellenorzes();
-
 }
 
 
 function fent(n) {
     szinValt(n - sorHossz);
-    console.log("fent");
 }
 
 function lent(n) {
     szinValt(n + sorHossz);
-    console.log("lent");
 }
 
 function bal(n) {
     szinValt(n - 1);
-    console.log("bal");
 }
 
 function jobb(n) {
     szinValt(n + 1);
-    console.log("jobb");
 }
 
 
 function szinValt(index) {
-
     let elem = $(".lampa")[index];
-    console.log(elem);
-
-    //let alapSzin = elem.css("background-color");
     let alapSzin = elem.style.backgroundColor;
-
     let szin = leKapcsolva;
+    
     if (alapSzin == leKapcsolva) {
         szin = felKapcsolva
-
     }
-    //elem.css("background-color", szin);
     elem.style.backgroundColor = szin
 
 }
 
 function ellenorzes() {
     let n = 0;
-    let elemek =  $(".lampa");
-    while(n < elemek.length && (elemek[n].style.backgroundColor == felKapcsolva)){
+    let elemek = $(".lampa");
+    while (n < elemek.length && (elemek[n].style.backgroundColor == felKapcsolva)) {
         n++;
     }
     if (!(n < elemek.length)) {
