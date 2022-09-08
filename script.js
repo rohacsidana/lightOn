e10a64f53fe341b709c3bf09acbd95ab26d5cc28
BerenyiPeti <berenyipeti01@gmail.com>
2022-09-08 21:28:53 +0200

    generálás_kész

--- script.js
+++ script.js
@@ -4,31 +4,46 @@ function init() {
     gombok()
 }
 
+
 function gombok() {
-    console.log("halo");
+    console.log("szia :)");
     document.getElementById("2").addEventListener("click", function () {
-        palya(4)
+        palya(4, "x2")
     })
     document.getElementById("3").addEventListener("click", function () {
-        palya(9)
+        palya(9, "x3")
     })
     document.getElementById("4").addEventListener("click", function () {
-        palya(16)
+        palya(16, "x4")
     })
     document.getElementById("5").addEventListener("click", function () {
-        palya(25)
+        palya(25, "x5")
     })
 
 }
 
-function palya(n) {
+function palya(n, osztaly) {
     let txt = ""
+    let zoldek = 0
     for (let index = 0; index < n; index++) {
-        txt += `<div class="lampa"></div>`
+        let random = Math.random()
+        if (random <= 0.5 && zoldek < n / 2) {
+            txt += `<div class="lampa" style="background-color: green;"></div>`
+            zoldek += 1
+        } else {
+            txt += `<div class="lampa" style="background-color: red;"></div>`
+
+        }
 
     }
 
+    document.getElementById("jatek").innerHTML = `<div id="fotarolo" class="${osztaly}"></div>`
     document.getElementById("fotarolo").innerHTML = txt
+    randomFel(n)
+}
+
+function randomFel(n) {
+
 }
 
 let sorHossz;
